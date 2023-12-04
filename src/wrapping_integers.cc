b62e21d9b72d513de90db73a4f363c9f6ec6118f
jia.xu <jia.xu@shopee.com>
2023-12-04 15:33:34 +0800

    wrap, unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -10,11 +10,9 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   uint64_t abs_seq1 = uint64_t(raw_value_ - zero_point.raw_value_) + (checkpoint / (1LL << 32)) * (1LL << 32);
+  if (abs_seq1 >= checkpoint)
+    abs_seq1 -= (1LL << 32);
   uint64_t abs_seq2 = abs_seq1 + (1LL << 32);
-  if (abs_seq1 >= checkpoint) {
-    abs_seq2 = abs_seq1;
-    abs_seq1 -= (1ul << 32);
-  }
   if (abs_seq2 - checkpoint < checkpoint - abs_seq1)
     return abs_seq2;
   return abs_seq1;
