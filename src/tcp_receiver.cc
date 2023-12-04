87d6316f8bd303ed53ae6324bc9e942ae7c7bef8
jia.xu <jia.xu@shopee.com>
2023-12-04 17:48:35 +0800

    reciver

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,18 +6,19 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
 {
   if (message.SYN) {
     isn_ = message.seqno;
-    message.seqno = message.seqno + 1; // move to first_index
   }
-  uint64_t first_index = message.seqno.unwrap(isn_.value(), inbound_stream.bytes_pushed() + 1);
+  uint64_t abs_seqno = message.seqno.unwrap(isn_.value(), inbound_stream.bytes_pushed() + 1);
+  uint64_t first_index = abs_seqno - 1;
   reassembler.insert(first_index, message.payload.release(), message.FIN, inbound_stream);
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
-  // Your code here.
   TCPReceiverMessage message {};
-  if (isn_.has_value())
-    message.ackno = Wrap32::wrap(inbound_stream.bytes_pushed() + 1, isn_.value());
+  if (isn_.has_value()) {
+    auto abs_seqon = inbound_stream.bytes_pushed() + 1 + inbound_stream.is_closed();
+    message.ackno = Wrap32::wrap(abs_seqon, isn_.value());
+  }
   auto buffer_sz = inbound_stream.available_capacity();
   message.window_size = buffer_sz > UINT16_MAX ? UINT16_MAX : buffer_sz;
   return message;
