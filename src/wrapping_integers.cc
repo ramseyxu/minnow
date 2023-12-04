d7ae0ced43afceb96d07aba1eee003bee970eb30
jia.xu <jia.xu@shopee.com>
2023-12-04 15:29:15 +0800

    wrap, unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,13 +4,13 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { uint32_t(n - 1 + zero_point.raw_value_) };
+  return Wrap32 { uint32_t(n + zero_point.raw_value_) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  uint64_t abs_seq1 = uint64_t(raw_value_ - zero_point.raw_value_ + 1) + checkpoint % (1ul << 32);
-  uint64_t abs_seq2 = abs_seq1 + (1ul << 32);
+  uint64_t abs_seq1 = uint64_t(raw_value_ - zero_point.raw_value_ + 1) + checkpoint % (1LL << 32);
+  uint64_t abs_seq2 = abs_seq1 + (1LL << 32);
   if (abs_seq1 >= checkpoint) {
     abs_seq2 = abs_seq1;
     abs_seq1 -= (1ul << 32);
